4d87cf3bc9cbca0ff860e9ea6aa5a2a28f3c0dd1
Artem Ruts <justartem@gmail.com>
2013-04-02 16:53:35 +0300

    Fixes memory allocation
    Fixes memory allocation: malloc(sizeof(char)*info.size);

--- file_read.c
+++ file_read.c
@@ -11,7 +11,7 @@ struct file_info read_file(char * file_name)
  	// Opening file
  	pFile = fopen(file_name, "r");
  	if (pFile == NULL) {
-		fputs("Opening file error", stderr);
+		fputs("Can't open file.", stderr);
 		exit(1);
   	}
 
@@ -23,14 +23,14 @@ struct file_info read_file(char * file_name)
 	// Allocating memory for storing file
 	info.content = (char*)malloc(sizeof(char)*info.size);
 	if (info.content == NULL) {
-		fputs("Memory error", stderr);
+		fputs("Memory Error.", stderr);
 		exit(2);
 	}
 
 	// Reading file
 	result = fread(info.content, 1, info.size, pFile);
 	if (result != info.size) {
-		fputs("Reading error", stderr);
+		fputs("Error. Can't read.", stderr);
 		exit(3);
 	}
 	fclose(pFile);
